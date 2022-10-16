93a41321f9d35168bbee5ca0cb1230a534804950
Divyash Chhetri <divyashchhetri@gmail.com>
2022-10-17 05:51:07 +0600

    Home and Search Page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,20 +11,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var flightNumber = ["KB150", "B3701", "B3700"];
-  var flightDuration = ["04 hr 10 min", "02 hr 30 min", "01 hr 05 min"];
-  var origin = ["PBH - Paro", "BKK - Bangkok", "PBH - Paro"];
-  var departure = ["0830", "0630", "1030"];
-  var destination = ["BKK - Bangkok", "CCU - Kolkata", "CCU - Kolkata"];
-  var arrival = ["1240", "0930", "1140"];
-  var date = ["14-09-2022", "19-09-2022", "23-09-2022"];
-  var airline = ["Druk Air", "Bhutan Airlines", "Bhutan Airlines"];
-  var imgList = [
-    "assets/images/druk_air_logo.png",
-    "assets/images/bhutan_airlines_logo.png",
-    "assets/images/bhutan_airlines_logo.png"
-  ];
-  var schedule_img = "assets/images/path.png";
+  var pathImg = "assets/images/path.png";
+  var drukAirLogo = "assets/images/druk_air_logo.png";
+  var bhutanAirlinesLogo = "assets/images/bhutan_airlines_logo.png";
 
   @override
   Widget build(BuildContext context) {
@@ -34,120 +23,169 @@ class _HomePageState extends State<HomePage> {
             elevation: 0,
             centerTitle: true,
             backgroundColor: Colors.red),
-        body: ListView.builder(
-            itemCount: flightNumber.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  print(flightNumber[index]);
-                },
-                child: Card(
-                    color: Color.fromARGB(255, 243, 236, 232),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+        body: ListView(children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 243, 236, 232),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            height: 220,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+                      child: Image.asset(
+                        drukAirLogo,
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: Image.asset(
-                            imgList[index],
-                            fit: BoxFit.cover,
-                          ),
-                          padding: const EdgeInsets.only(top: 35, bottom: 125),
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                flightNumber[index],
-                                style: const TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              padding: const EdgeInsets.only(
-                                  left: 10, top: 30, bottom: 40, right: 30),
-                            ),
-                            Container(
-                              child: Text(
-                                departure[index],
-                                style: const TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              padding: const EdgeInsets.only(
-                                  top: 0, bottom: 10, right: 30),
-                            ),
-                            Container(
-                              child: Text(
-                                origin[index],
-                                style: const TextStyle(
-                                    fontSize: 10, fontWeight: FontWeight.bold),
-                              ),
-                              padding: const EdgeInsets.only(
-                                  left: 10, bottom: 40, right: 30),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                flightDuration[index],
-                                style: const TextStyle(
-                                  fontSize: 13,
-                                  fontFamily: 'a charming font',
-                                ),
-                              ),
-                              padding: const EdgeInsets.only(
-                                  left: 0, top: 30, bottom: 50),
-                            ),
-                            Container(
-                              child: Text(
-                                date[index],
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                ),
-                              ),
-                              padding:
-                                  const EdgeInsets.only(left: 0, bottom: 30),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Container(
-                              width: 50,
-                              height: 100,
-                              child: Image.asset(
-                                schedule_img,
-                                fit: BoxFit.cover,
-                              ),
-                              padding: const EdgeInsets.only(
-                                  right: 10, left: 0, top: 30, bottom: 50),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                arrival[index],
-                                style: const TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              padding: const EdgeInsets.only(left: 40, top: 70),
-                            ),
-                            Container(
-                              child: Text(
-                                destination[index],
-                                style: const TextStyle(
-                                    fontSize: 10, fontWeight: FontWeight.bold),
-                              ),
-                              padding: const EdgeInsets.only(left: 40, top: 10),
-                            ),
-                          ],
-                        )
-                      ],
-                    )),
-              );
-            }));
+                    Container(
+                        margin: const EdgeInsets.only(left: 20, top: 7),
+                        child: const Text(
+                          'KB150',
+                          style: TextStyle(
+                              fontSize: 19.0, fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, top: 20),
+                  child: const Text('04 hr 10 min',
+                      style: TextStyle(fontSize: 15, color: Colors.black)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10.0),
+                        child: const Text('08:30',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      const Text('CCU - Kolkata',
+                          style: TextStyle(fontSize: 15)),
+                    ]),
+                    Container(
+                      margin: const EdgeInsets.only(left: 15, right: 15),
+                      child: Image.asset(
+                        pathImg,
+                        fit: BoxFit.cover,
+                        height: 25,
+                        width: 100,
+                      ),
+                    ),
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10.0),
+                        child: const Text('12:40',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      const Text('BKK - Bangkok',
+                          style: TextStyle(fontSize: 15)),
+                    ]),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, top: 20),
+                  child:
+                      const Text('18/10/2022', style: TextStyle(fontSize: 15)),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 243, 236, 232),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            height: 220,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+                      child: Image.asset(
+                        drukAirLogo,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(left: 20, top: 7),
+                        child: const Text(
+                          'KB150',
+                          style: TextStyle(
+                              fontSize: 19.0, fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, top: 20),
+                  child: const Text('04 hr 10 min',
+                      style: TextStyle(fontSize: 15, color: Colors.black)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10.0),
+                        child: const Text('08:30',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      const Text('CCU - Kolkata',
+                          style: TextStyle(fontSize: 15)),
+                    ]),
+                    Container(
+                      margin: const EdgeInsets.only(left: 15, right: 15),
+                      child: Image.asset(
+                        pathImg,
+                        fit: BoxFit.cover,
+                        height: 25,
+                        width: 100,
+                      ),
+                    ),
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10.0),
+                        child: const Text('12:40',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      const Text('BKK - Bangkok',
+                          style: TextStyle(fontSize: 15)),
+                    ]),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, top: 20),
+                  child:
+                      const Text('18/10/2022', style: TextStyle(fontSize: 15)),
+                ),
+              ],
+            ),
+          )
+        ]));
   }
 }
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -8,13 +8,125 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  var pathImg = "assets/images/path.png";
+  var drukAirLogo = "assets/images/druk_air_logo.png";
+  var bhutanAirlinesLogo = "assets/images/bhutan_airlines_logo.png";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: const Text('SEARCH FLIGHTS'),
-      centerTitle: true,
-      backgroundColor: Colors.red,
-    ));
+          title: const Text('SEARCH FLIGHTS'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: ListView(children: <Widget>[
+          Container(
+              height: 160,
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+              child: Column(children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Search Flights...',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child:
+                          const Text('Search', style: TextStyle(fontSize: 16)),
+                    ),
+                  ),
+                ),
+              ])),
+          Container(
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 243, 236, 232),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            height: 220,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 10.0, top: 10.0),
+                      child: Image.asset(
+                        drukAirLogo,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(left: 20, top: 7),
+                        child: const Text(
+                          'KB150',
+                          style: TextStyle(
+                              fontSize: 19.0, fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, top: 20),
+                  child: const Text('04 hr 10 min',
+                      style: TextStyle(fontSize: 15, color: Colors.black)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10.0),
+                        child: const Text('08:30',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      const Text('CCU - Kolkata',
+                          style: TextStyle(fontSize: 15)),
+                    ]),
+                    Container(
+                      margin: const EdgeInsets.only(left: 15, right: 15),
+                      child: Image.asset(
+                        pathImg,
+                        fit: BoxFit.cover,
+                        height: 25,
+                        width: 100,
+                      ),
+                    ),
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Column(children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10.0),
+                        child: const Text('12:40',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      const Text('BKK - Bangkok',
+                          style: TextStyle(fontSize: 15)),
+                    ]),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, top: 20),
+                  child:
+                      const Text('18/10/2022', style: TextStyle(fontSize: 15)),
+                ),
+              ],
+            ),
+          )
+        ]));
   }
 }
