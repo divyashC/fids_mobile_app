15279130ae9a0e6a16d7deaf7920e05608639dc7
Divyash Chhetri <divyashchhetri@gmail.com>
2022-10-17 07:53:51 +0600

    Reminders

--- lib/reminder_page.dart
+++ lib/reminder_page.dart
@@ -11,97 +11,131 @@ class _ReminderPageState extends State<ReminderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('REMINDERS'),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Container(
-            margin: const EdgeInsets.only(top: 120, left: 30),
-            // add sizedbox from the code below
-            child: SizedBox(
-              width: 366,
-              height: 145,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Container(
-                          width: 366,
-                          height: 145,
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(25),
-                              topRight: Radius.circular(25),
-                              bottomLeft: Radius.circular(25),
-                              bottomRight: Radius.circular(25),
-                            ),
-                            color: Color.fromRGBO(255, 242, 242, 1),
-                          ))),
-                  const Positioned(
-                      top: 116,
-                      left: 17,
-                      child: Text(
-                        'PBH - BKK ',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Color.fromRGBO(85, 85, 85, 1),
-                            fontFamily: 'Helvetica',
-                            fontSize: 14,
-                            letterSpacing:
-                                0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1.1428571428571428),
-                      )),
-                  const Positioned(
-                      top: 96,
-                      left: 20,
-                      child: Text(
-                        'Druk Air - KB150',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Color.fromRGBO(85, 85, 85, 1),
-                            fontFamily: 'Helvetica',
-                            fontSize: 14,
-                            letterSpacing:
-                                0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1.1428571428571428),
-                      )),
-                  const Positioned(
-                      top: 72,
-                      left: 20,
-                      child: Text(
-                        '14-09-2022',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            color: Color.fromRGBO(25, 25, 25, 1),
-                            fontFamily: 'Helvetica',
-                            fontSize: 17,
-                            letterSpacing:
-                                0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 0.9411764705882353),
-                      )),
-                  const Positioned(
-                      top: 19,
-                      left: 14,
-                      child: Text(
+      appBar: AppBar(
+        title: const Text('REMINDERS'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: ListView(children: <Widget>[
+        Container(
+            height: 160,
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 243, 236, 232),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Row(children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(left: 5, top: 5),
+                      child: const Text(
                         '08:30 AM',
-                        textAlign: TextAlign.center,
                         style: TextStyle(
-                            color: Color.fromRGBO(0, 0, 0, 1),
-                            fontFamily: 'Helvetica',
-                            fontSize: 40,
-                            letterSpacing:
-                                0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1),
-                        // close all brackets
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  Container(
+                    margin: const EdgeInsets.only(left: 5),
+                    child: const Text('20/10/2022',
+                        style: TextStyle(fontSize: 19.0)),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(left: 5, top: 15),
+                      child: const Text(
+                        'Druk Air - KB150',
+                        style: TextStyle(fontSize: 15.0, color: Colors.black54),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      child: const Text(
+                        'PBH - BKK ',
+                        style: TextStyle(fontSize: 15.0, color: Colors.black54),
                       ))
                 ],
               ),
-            )));
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 100),
+                    child: Switch(
+                      value: true,
+                      onChanged: (value) {},
+                      activeTrackColor: Colors.redAccent,
+                      activeColor: Colors.red,
+                    ),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(left: 100, top: 50),
+                      child: const Icon(Icons.more_horiz_outlined,
+                          size: 40, color: Colors.redAccent))
+                  // add icon
+                ],
+              ),
+            ])),
+        Container(
+            height: 160,
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 243, 236, 232),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Row(children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(left: 5, top: 5),
+                      child: const Text(
+                        '02:30 PM',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  Container(
+                    margin: const EdgeInsets.only(left: 5),
+                    child: const Text('28/10/2022',
+                        style: TextStyle(fontSize: 19.0)),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(left: 5, top: 15),
+                      child: const Text(
+                        'Bhutan Airlines - B3701',
+                        style: TextStyle(fontSize: 15.0, color: Colors.black54),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      child: const Text(
+                        'BKK - CCU',
+                        style: TextStyle(fontSize: 15.0, color: Colors.black54),
+                      ))
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 100),
+                    child: Switch(
+                      value: true,
+                      onChanged: (value) {},
+                      activeTrackColor: Colors.redAccent,
+                      activeColor: Colors.red,
+                    ),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(left: 100, top: 50),
+                      child: const Icon(Icons.more_horiz_outlined,
+                          size: 40, color: Colors.redAccent))
+                  // add icon
+                ],
+              ),
+            ]))
+      ]),
+    );
   }
 }
